59214b2af344636b647a8ed5f752c12693466423
Nick Chang <darkcloud71962@gmail.com>
2021-05-25 10:11:45 +0800

    first commit

---
+++ EmailApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,69 @@
+﻿using EmailService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IEmailSender _emailSender;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IEmailSender emailSender)
+        {
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var rng = new Random();
+
+            //var message = new Message(new string[] { "shenghuai0719@gmail.com" }, "Test email", " This is the content from our email.");
+            //_emailSender.SendEmail(message);
+
+            var message = new Message(new string[] { "shenghuai0719@gmail.com" }, "Test email async", "This is the content from our email.", null);
+            await _emailSender.SendEmailAsync(message);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public async Task<IEnumerable<WeatherForecast>> Post()
+        {
+            var rng = new Random();
+
+            var files = Request.Form.Files.Any() ? Request.Form.Files : new FormFileCollection();
+
+            var message = new Message(new string[] { "shenghuai0719@gmail.com" }, "NETCore MailKit 測試", "NETCore MailKit 測試信", files);
+
+            await _emailSender.SendEmailAsync(message);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
